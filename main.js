f8e5b082581278a330de1cbae921a13c2c660db8
duqian91 <duqian91@users.noreply.github.com>
2016-06-27 22:24:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
-// link user input to searchQuery
-var searchQuery = "";
-$.ajax({
-  url: '//en.wikipedia.org/w/api.php',
-  data: {
-    action: 'query',
-    list: 'search',
-    srsearch: "canada", // NEED TO CHANGE TO USER INPUT
-    format: 'json'
-  },
-  dataType: 'jsonp',
-  success: function(data) {
-    // link html to search results in a list? 
-    // make a for loop
-    var searchResult = data.query.search;
-    // var displaySearchResultTitle = [];
-    var searchMap = {};
-    var displayHTMLResult = "";
-    for (var i = 0; i < searchResult.length; i++) {
-      displayHTMLResult += "<h2><a href=\"http://en.wikipedia.org/wiki/" + searchResult[i].title + "\">" + searchResult[i].title + "</a></h2>" +
-        "<p>" + searchResult[i].snippet + "</p>";
-      // displaySearchResultTitle.push(searchResult[i].title);
-      // searchMap[searchResult[i].title] = searchResult[i].snippet;
-    }
-
-    function displaySearchMap() {
-      for (var key in searchMap) {
+$("#userQuery").keyup(function() {
+  // get user input in realtime when user enters something
+  var searchQuery = $("#userQuery").val();
+  $.ajax({
+    url: '//en.wikipedia.org/w/api.php',
+    data: {
+      action: 'query',
+      list: 'search',
+      srsearch: searchQuery,
+      format: 'json'
+    },
+    dataType: 'jsonp',
+    success: function(data) {
+      // the api tree is data.query.search
+      var searchResult = data.query.search;
+      var displayHTMLResult = "";
+      // iterate through all the different objects
+      for (var i = 0; i < searchResult.length; i++) {
+        // push to HTML result
+        displayHTMLResult +=
+          "<h2>" +
+          "<a href=\"http://en.wikipedia.org/wiki/" + searchResult[i].title + "\" target=\"_blank\">" + searchResult[i].title + "</a>" +
+          "</h2>" +
+          "<p>" + searchResult[i].snippet + "</p>";
       }
+      $("#display").html(displayHTMLResult);
     }
-    $("#display").html(displayHTMLResult);
-    console.log(searchMap);
-  }
+  });
 });
